74d8b604ba7aac162c252e92f7d4ba981a501837
Joao-M-Bastos <jmb32180@gmail.com>
2023-04-30 17:30:05 -0300

    MAISSSSSSSSSSSS

--- IFTP Corps/Assets/Resourses/Scripts/ButtonScrpt.cs
+++ IFTP Corps/Assets/Resourses/Scripts/ButtonScrpt.cs
@@ -5,8 +5,34 @@ using UnityEngine;
 public class ButtonScrpt : MonoBehaviour
 {
     [SerializeField]DeskBehavious dbInstance;
+
+    [SerializeField] GameObject buttonEsclude, buttonUpgrade;
+
+    bool clickedInButton;
+
+    private void Awake()
+    {
+        ActiveBools(false);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonUp(0) && !clickedInButton)
+        {
+            ActiveBools(false);
+        }
+    }
+
     public void OnOpenClick()
     {
+        ActiveBools(true);
         dbInstance.SelectDesc();
+    }
+
+    public void ActiveBools(bool b)
+    {
+        clickedInButton = b;
+        buttonEsclude.SetActive(b);
+        buttonUpgrade.SetActive(b);
     }
 }
